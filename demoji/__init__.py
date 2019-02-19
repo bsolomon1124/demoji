c0acba5cf21b46feae30b23151be8ff840566274
Brad Solomon <brad.solomon.1124@gmail.com>
2019-02-18 20:41:06 -0500

    Only specify pytest in tox environment reqs

--- demoji/__init__.py
+++ demoji/__init__.py
@@ -10,7 +10,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 __all__ = ("findall", "replace", "last_downloaded_timestamp")
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 import datetime
 import json

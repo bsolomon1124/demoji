17d87090b2521d396e02b4e116829d723c432a8c
Brad Solomon <bsolomon@protonmail.com>
2020-04-14 16:34:19 -0400

    Version 0.2.1

--- demoji/__init__.py
+++ demoji/__init__.py
@@ -10,7 +10,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 __all__ = ("findall", "replace", "last_downloaded_timestamp")
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 import datetime
 import json

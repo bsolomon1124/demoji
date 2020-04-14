480ce621ed402fa24e2832bbda66df6a9f49c751
Brad Solomon <bsolomon@protonmail.com>
2020-04-14 16:41:18 -0400

    Add PyPI classifier for Python 3.8

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     install_requires=["requests<3.0.0", "setuptools", "colorama"],

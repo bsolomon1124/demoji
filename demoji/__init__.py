c949ada2e237bebafd282febc1973d069cd9e29d
Brad Solomon <bsolomon@protonmail.com>
2020-04-14 16:41:06 -0400

    Bump devel version

--- demoji/__init__.py
+++ demoji/__init__.py
@@ -10,7 +10,7 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 __all__ = ("findall", "replace", "last_downloaded_timestamp")
-__version__ = "0.2.1"
+__version__ = "0.2.2-dev0"
 
 import datetime
 import json
